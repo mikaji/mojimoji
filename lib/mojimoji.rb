6f437a431f97c387803f2e22ffc291122c70916f
Koji Mikami <kody.j.gigs@gmail.com>
2018-06-15 22:14:55 +0900

    Add methods

--- lib/mojimoji.rb
+++ lib/mojimoji.rb
@@ -1,5 +1,34 @@
 require "mojimoji/version"
 
 module Mojimoji
-  # Your code goes here...
+  refine String do
+
+    def strip_all_spaces_and_to_em
+      strip_all_spaces.to_em
+    end
+
+    def strip_all_spaces
+      gsub(/[[:space:]]+/, '')
+    end
+
+    def to_em
+      Moji.han_to_zen(self.backquote_to_em)
+    end
+
+    def backquote_to_em
+      gsub('`', '｀')
+    end
+
+    def strip_all_spaces_and_to_em?
+      em? && strip_all_spaces?
+    end
+
+    def em?
+      Moji.type?(self, Moji::ZEN)
+    end
+
+    def strip_all_spaces?
+      (/[[:space:]]+/ =~ self) == nil
+    end
+  end
 end
